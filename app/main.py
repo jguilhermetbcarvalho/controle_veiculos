109bebca37c4b172636eff01a028f92814fa00be
jguilhermetbcarvalho <joaogcarvalho012@gmail.com>
2023-12-05 08:06:14 -0400

    CLEAN: Tirando comentários

--- app/main.py
+++ app/main.py
@@ -1,11 +1,2 @@
 
 from infrastructure.database import setup_database
-
-# # Exemplo de uso para salvar veículo
-# database_operations.salvar_entidade('veiculos', placa='ABC123', modelo='Modelo1', ano=2022)
-
-# # Exemplo de uso para salvar abastecimento
-# database_operations.salvar_entidade('abastecimentos', data='2023-01-01', litros=50, valor=200.0)
-
-# # Exemplo de uso para salvar uso
-# database_operations.salvar_entidade('usos', data='2023-01-02', distancia=100, veiculo_id=1)
